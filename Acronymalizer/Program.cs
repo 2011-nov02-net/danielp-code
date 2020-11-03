dfc2cc39da109c6bdc29b7f33a0f796c2ba1be14
peter346 <peter346@wwu.edu>
2020-11-03 13:26:29 -0800

    Made Acronym uppercase

--- Acronymalizer/Program.cs
+++ Acronymalizer/Program.cs
@@ -6,7 +6,7 @@ namespace Acronymalizer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a phrase");
+            Console.WriteLine("Enter a phrase.");
             String userInput = Console.ReadLine();
 
             String[] words = userInput.Split(" ");
@@ -15,13 +15,20 @@ namespace Acronymalizer
 
             for( int i = 0; i < words.Length; i++){
 
+                //skip any enteries that are empty
                 if(words[i].Length > 0)
                 {
+                    //avoid leading spaces
                     words[i] = words[i].Trim();
+
+                    //append the character to the acronym
                     Acronym = Acronym + words[i][0];
                 }
                 
             }
+
+            //make the acronym upper case like real acronyms
+            Acronym = Acronym.ToUpper();
             Console.WriteLine(Acronym);
         }
     }
