b5f7d93d0a44a3eda087c16812f13c673e3b448c
peter346 <peter346@wwu.edu>
2020-11-03 13:01:28 -0800

    Triangel nested loop, and echo

--- SecondDotNet/SecondProject/Program.cs
+++ SecondDotNet/SecondProject/Program.cs
@@ -2,11 +2,39 @@
 
 namespace SecondProject
 {
+    // Projects contain classes
+    // Classes contain members like methods and fields
+
+
     class Program
     {
+        //static main method like Java for console to run
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Is there an echo in here?");
+
+
+            // C# is (strongly) typed
+
+            //unlike java, no like scanners or input streams neccisssary 
+            String userInput = Console.ReadLine();
+            Console.WriteLine(userInput);
+
+
+            //look up switch expression, does pattern matching.
+
+            int length = 4;
+
+            //triangle
+            for (int i = 0; i <= length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine("");
+            }
+            
         }
     }
 }
