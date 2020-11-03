e51c0f9e656f3c9b867f6c281398d8d671b561c1
peter346 <peter346@wwu.edu>
2020-11-03 13:21:49 -0800

    Inital acronymalizer

---
+++ Acronymalizer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Acronymalizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a phrase");
+            String userInput = Console.ReadLine();
+
+            String[] words = userInput.Split(" ");
+
+            String Acronym = new String("");
+
+            for( int i = 0; i < words.Length; i++){
+
+                if(words[i].Length > 0)
+                {
+                    words[i] = words[i].Trim();
+                    Acronym = Acronym + words[i][0];
+                }
+                
+            }
+            Console.WriteLine(Acronym);
+        }
+    }
+}
