2ac6398b0b3fa86ca3614378046fd99a14b2cb4a
peter346 <peter346@wwu.edu>
2020-11-03 17:58:44 -0800

    Added + Integrated Transaction Class

--- Bank/Program.cs
+++ Bank/Program.cs
@@ -8,6 +8,18 @@ namespace Bank
         {
             var account = new BankAccount("<name>", 1000);
             Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+
+            // Test that the initial balances must be positive.
+            try
+            {
+                var invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
