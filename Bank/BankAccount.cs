052db44987557f8d2aa1c129e79f2474f81789a5
peter346 <peter346@wwu.edu>
2020-11-05 12:27:19 -0800

    minor notes

--- Bank/BankAccount.cs
+++ Bank/BankAccount.cs
@@ -6,8 +6,14 @@ namespace Bank
 {
     public class BankAccount
     {
+        //could be b/c it's (1) immutable, and (2) not ment to act like a number
+        //it's more like a string label or an id, that's just made of numbers only.
         public string Number { get; }
         public string Owner { get; set; }
+
+        //lets you varify the accuracy of the balance based on the transactions
+        //and also lets you track transactions.
+        //storing the balance seperately, then you have another point of failure.
         public decimal Balance
         {
             get
