13be19a412a03ff198819bdfdde94839e944388f
Noelwiz <noelwiz1@gmail.com>
2020-12-10 07:41:04 -0800

    more template projects created to follow along

---
+++ HelloWebApi.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace HelloWebApi.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        //arrive here for get requests to WeatherForcast, recall that the controller's route is basically
+        // like prefixed on methods
+
+        //ALSO note we return c# object not like something else
+        //will be defaault serialized as JSON files.
+
+        //the serialized data is intended more for code than it is for users.
+        //easy to test get methods w/ just like a browser, but not very human friendly because it's just json
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
